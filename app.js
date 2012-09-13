e84bc5938808f99466ba91204b12c9ac42c4ad51
Peter Wooley <peterwooley@gmail.com>
2012-09-12 23:41:06 -0700

    Added real-time 2 player action! Note: This is extremely rough code; don't try to run any mission-critical paper wars games on it.

--- app.js
+++ app.js
@@ -31,6 +31,50 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+
+var io = socketIO.listen(server);
+var player1 = null;
+var player2 = null;
+
+io.sockets.on('connection', function(socket) {
+  if(!player1) {
+    player1 = socket;
+    socket.emit('game-pending', {message: "You are the first player to join. Please wait for Player 2."});
+  } else if(!player2) {
+    player2 = socket;
+    player1.emit('game-start', {role:"army"});
+    player2.emit('game-start', {role:"bomber"});
+  } else {
+    throw new Error("There are too many players right now!");
+  }
+
+  // When a player sends an event saying it is done (e.g., after the user has selected
+  // 5 checkboxes), save the coords the user sent.
+  socket.on('player-done', function(data) {
+    if(socket === player1 || socket === player2) {
+      socket.coords = data.coords;
+    }
+
+    // Compare the two player's coords to see if Player 2 got any points.
+    if(player1.coords && player2.coords) {
+      var hits = [];
+      player2.coords.forEach(function(item) {
+        var hit = player1.coords.indexOf(item) !== -1;
+        if(hit) {
+          hits.push(item);
+        }
+      });
+      // Send an event telling the clients the game is over and which coords were hits.
+      io.sockets.emit('game-over', {hits: hits});
+    }
+
+    console.log("player-done coords:", data);
+  });
+});
+
+
+
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
